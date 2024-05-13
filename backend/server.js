d054b3e17033357bc00bffdddec511a6e9467dfb
joaogustavo <joaogustavone@gmail.com>
2024-05-13 14:09:56 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,142 @@
+const todos = [
+    {
+        action: "Wake up",
+        dueDate: new Date("2024-05-10"),
+        completed: true
+    },
+    {
+        action: "Take a shower",
+        dueDate: new Date("2024-05-11"),
+        completed: true
+    },
+    {
+        action: "Feed the cats",
+        dueDate: new Date("2024-05-12"),
+        completed: true
+    },
+    {
+        action: "Have a cuppa",
+        dueDate: new Date("2024-05-13"),
+        completed: false
+    }
+]
+
+
+//import express and cors
+const express = require('express')
+const cors =  require('cors')
+
+const app = express()
+app.use(cors())
+
+const PORT = 3001
+
+
+
+//building an endpoint
+app.get("/hello", (req,res) => {
+    //adding the status and sending the data
+    res.status(200).send("<h2>Hello, World!</h2>")
+})
+
+//building the todo endpoint
+app.get("/todos", (req,res) => {
+    res.status(200).json(todos)
+})
+
+//building a specified todo from the todos array
+// app.get("/todos/:id", (req,res) => {
+//     const id = req.params.id
+//     const todo = todos[id - 1]
+//     res.status(200).json(todo)
+// })
+
+
+
+//error handler
+let todoNotFoundError = {
+    status: 404,
+    message: "Todo not found!"
+}
+
+//building a specified todo from the json
+app.get("/todos/:id", (req,res) => {
+    let id = req.params.id - 1
+    if (id >= todos.length || id < 0){
+        res.status(404)
+        res.json(todoNotFoundError)
+        return
+    }
+    res.status(200)
+    res.json(todos[id])
+})
+
+//end point todos
+app.get("/todosbyaction/:action", (req,res) => {
+    let action = req.params.action
+    let index = todos.findIndex(x => x.action.toLocaleLowerCase() == action)
+    if(index == -1){
+        res.status(404).json(todoNotFoundError)
+        return
+    }
+    res.status(200).json(todos[index])
+})
+
+
+//date error handlers
+let dateNotFormattedError = {
+    status: 404,
+    message: "Date is not properly formatted, please ensure you're using YYYY-MM-DD"
+}
+
+let todosNotFoundError = {
+    status: 404,
+    message: "Could not find any todos with the given condition"
+}
+
+//end point todos by date from URL
+app.get("/todosbydate/:dueDate", (req,res) => {
+    let date = Date.parse(req.params.dueDate)
+    if(date == NaN){
+        res.status(404).json(dateNotFormattedError)
+        return
+    }
+    let datedArray = todos.filter(x => x.dueDate.getTime() == date.getTime())
+    if(datedArray.length == 0){
+        res.status(404).json(todosNotFoundError)
+        return
+    }
+    res.status(200).json(datedArray)
+})
+
+//end point todo status
+app.get("/todosbycompleted/:completed", (req,res) => {
+    let completedBool = false
+    if(req.params.completed.toLocaleLowerCase() == "true"){
+        completedBool = true
+    }
+    let filteredArray = todos.filter(x => x.completed == completedBool)
+    if(filteredArray.length == 0){
+        res.status(404).json(todoNotFoundError)
+        return
+    }
+    res.status(200).json(filteredArray)
+})
+
+//end point todo updating the status
+app.get("/completetodo/:id", (req,res) => {
+    let id = req.params.id
+    if(id >= todos.length || id < 0){
+        res.status(404).json(todoNotFoundError)
+        return
+    }
+    todos[id].completed = !todos[id].completed
+    res.status(200).json(todos[id])
+})
+
+
+//
+app.listen(PORT, () => {
+    //console.log followed by the colour and the message
+    console.log('\x1b[32m%s\x1b[0m',`🟢  Server is listening on http://localhost:${PORT}`)
+})
